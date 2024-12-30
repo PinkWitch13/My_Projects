0f1f86c1ebd92766af33b375236d0d7213e038a6
PinkWitch13 <pinkwitch13@proton.me>
2024-12-30 18:13:22 +0100

    adding my first Python project: printing in command line clock like graphic and actual date and time

---
+++ clock_project.py
@@ -0,0 +1,10 @@
+import datetime
+
+print("    |")
+print("  \\   /")
+print(" -- .---")
+print("  /   \\")
+print("    |")
+
+current_time= datetime.datetime.now()
+print("Current time in Warsav is:", current_time)
